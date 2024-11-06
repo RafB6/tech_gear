192835264cd8b187ad300596ebbfdfa220c0cf3d
r4xon <r4xon@Linux.myguest.virtualbox.org>
2024-11-06 18:09:03 +0100

    Switching to strictly client-side data updating to improve efficiency and decrease server calls

--- x-com.py
+++ x-com.py
@@ -16,35 +16,8 @@ def gear():
 
 @app.route("/get_gear", methods=["GET"])
 def get_gear():
-    #get arguments from user
-    sort_by = request.args.get('sort_by', 'id')
-    reverse = request.args.get('reverse', 'normal')
-    query = request.args.get('query', 'default')
-    if reverse == "normal":
-        rev = False
-    else:
-        rev = True
-
-    query_gear = []
-    if query != "default" and len(query) > 0 and query is not None:
-        for elem in gear_data:
-            if elem in query_gear:
-                continue
-
-            if query.lower() in str(elem["brand"]).lower() or query.lower() in str(elem["model"]).lower():
-                query_gear.append(elem)
-    else:
-        query_gear = gear_data
-    
-    #print values
-    if sort_by == "price":
-        sorted_gear = sorted(query_gear,key = lambda x: float(x[sort_by]),reverse=rev)
-    else:
-        sorted_gear = sorted(query_gear, key = lambda x: x[sort_by], reverse=rev)
-
     #return JSON to browser
-    return jsonify(sorted_gear)
-
+    return jsonify(gear_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
