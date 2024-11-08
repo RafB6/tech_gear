af222be85e57466c2b108249165a0fdc96521f32
r4xon <r4xon@Linux.myguest.virtualbox.org>
2024-11-08 22:26:43 +0100

    initialized dataValidor file, to be finished

--- static/scripts/data-validator.js
+++ static/scripts/data-validator.js
@@ -1,1 +1,6 @@
 //Check if username/password meets expectations.
+dataValidator = () => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+}
