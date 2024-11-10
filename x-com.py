9987a506ea7e87ff33ed4581c665d121c1482b9b
r4xon <r4xon@Linux.myguest.virtualbox.org>
2024-11-10 19:28:39 +0100

    Added database connection

--- x-com.py
+++ x-com.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify, g
 import json
+import sqlite3
 
 app = Flask(__name__)
+app.config["DATABASE"] = "users.db"
 
+with open("item_data.json", "r") as data_file:
+    gear_data = json.load(data_file) 
+
+def database_connection():
+    if "db" not in g:
+        g.db = sqlite3.connect(app.config["DATABASE"])
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
+        
 @app.route("/")
 def main():
     return render_template("display/main_page.html")
 
-with open("item_data.json", "r") as data_file:
-    gear_data = json.load(data_file)
 
 @app.route("/gear")
 def gear():
